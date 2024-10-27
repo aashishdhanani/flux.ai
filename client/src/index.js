1f8bff7d2b0050d1f49208e920fe2507596d9800
LeviBetsy <leuykhiem@gmail.com>
2024-10-26 23:37:04 -0500

    grid

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './styles/index.css';
 import Login from './login';
 import Home from './home';  // Create this file
 import reportWebVitals from './reportWebVitals';
